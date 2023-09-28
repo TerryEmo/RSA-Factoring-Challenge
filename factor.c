d88715c1052e9adfa9fcbf9048763cbb5107eccb
TerryEmo <terrance.sabelo@gmail.com>
2023-09-28 12:13:28 -0700

    RSA Factoring Challenge

--- factor.c
+++ factor.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-/*
-    Finds the smallest divisor, if any, of a given number `n`
-    Returns:
-        smallest divisor if found
-        0 if n is prime
-*/
-int trial_division(long int n){
-long int f;
 
-if (n%2 == 0){
-	printf("%lu=%lu*%i\n", n, n/2, 2);
-	return 0;
-}
-
-f = 3;
-while (f*f <= n)
+int main()
 {
-	if (n%f == 0){
-		printf("%lu=%lu*%lu\n", n, n/f, f);
-		return 0;
-	}
-	else{
-		f += 2;
-	}
-}
-printf("%lu=%lu*%i\n", n, n, 1);
-return 0;
+    long long int num = 239809320265259;
+    long int factor1 = 2;
+    long int factor2;
+
+    while (num % factor1)
+    {
+        if (factor1 <= num)
+        {
+            factor1++;
+        }
+        else {
+            return (-1);
+        }
+    }
+
+    factor2 = num / factor1;
+    printf("%lld = %ld * %ld\n", num, factor2, factor1);
+    return (0);
 }
